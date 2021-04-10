aedf2a586619ee408186a77fceee5ea01d646533
Aytackydln <aytackayadelen@gmail.com>
2021-04-11 00:02:24 +0300

    fixed jsp rendering and topic name

--- src/main/java/com/noname/duyuru/app/jpa/models/Department.java
+++ src/main/java/com/noname/duyuru/app/jpa/models/Department.java
@@ -3,7 +3,6 @@ package com.noname.duyuru.app.jpa.models;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
@@ -13,7 +12,6 @@ import javax.persistence.PrimaryKeyJoinColumn;
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
-@ToString
 public class Department extends Topic {
     private String classesUri;
     private String classElementSelector;
